2a000f2cd7eae657f62e1ae79a181a06d0e3481c
Yevgen Yampolskiy <genij.math@gmail.com>
2015-01-11 17:03:16 -0800

    Initial implementation of a matrix with caching of the inverse support. This is a solution for "R Programming" course by Coursera which follows sample implementation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for calculation of inverse of an invertible matrix 
+## with support of caching.
+##
+## Sample usage:
+## > m <- makeCacheMatrix(matrix(1:4, 2, 2))
+## > cacheSolve(m)  # calculates inverse of the matrix m
+## > cacheSolve(m)  # second call returns inverse of the matrix m from the cache
+## > m$get() %*% cacheSolve(m)  # returns identity matrix
 
-## Write a short comment describing this function
+## Creates an object encapsulating matrix and a cached value.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cacheValue <- NULL
+  get <- function() x
+  setCachedInverse <- function(inv) cacheValue <<- inv
+  getCachedInverse <- function() cacheValue
+  list(get = get,
+       setCachedInverse = setCachedInverse,
+       getCachedInverse = getCachedInverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates inverse of the matrix wrapped by makeCacheMatrix matrix 
+## or returns cached value for the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cacheValue <- x$getCachedInverse()
+  if(!is.null(cacheValue)) {
+    message("getting cached data")
+    return(cacheValue)
+  }
+  matrix <- x$get()
+  cacheValue <- solve(matrix)
+  x$setCachedInverse(cacheValue)
+  cacheValue  
 }
